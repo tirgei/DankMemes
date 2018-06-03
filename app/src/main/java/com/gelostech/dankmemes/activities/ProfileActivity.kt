106fc4f8023cd88ea923af920cb122b4b699cec7
Tirgei <tirgeic@gmail.com>
2018-06-03 21:38:37 +0300

    added toolbar

--- app/src/main/java/com/gelostech/dankmemes/activities/ProfileActivity.kt
+++ app/src/main/java/com/gelostech/dankmemes/activities/ProfileActivity.kt
@@ -2,13 +2,34 @@ package com.gelostech.dankmemes.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.gelostech.dankmemes.R
+import com.gelostech.dankmemes.commoners.BaseActivity
+import kotlinx.android.synthetic.main.activity_profile.*
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(viewProfileToolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.profile)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+
+        return true
     }
 
     override fun onBackPressed() {
