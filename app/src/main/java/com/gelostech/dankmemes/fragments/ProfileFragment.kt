7532f8f302af1338a55e7617ab79d61e2dbd01c9
Tirgei <tirgeic@gmail.com>
2018-06-06 00:04:37 +0300

    updated item click listener

--- app/src/main/java/com/gelostech/dankmemes/fragments/ProfileFragment.kt
+++ app/src/main/java/com/gelostech/dankmemes/fragments/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.gelostech.dankmemes.fragments
 
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DefaultItemAnimator
@@ -56,7 +57,7 @@ class ProfileFragment : Fragment(), MemesAdapter.OnItemClickListener {
         memesAdapter.addMeme(meme3)
     }
 
-    override fun onItemClick(meme: MemeModel, viewID: Int) {
+    override fun onItemClick(meme: MemeModel, viewID: Int, image: Bitmap?) {
 
     }
 }
