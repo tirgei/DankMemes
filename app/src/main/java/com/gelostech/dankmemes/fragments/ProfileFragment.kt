accd05d96a8185d36a18edacdbe27b25764b0376
Tirgei <tirgeic@gmail.com>
2018-06-09 12:51:07 +0300

    added functionality to fetch profile from firebase

--- app/src/main/java/com/gelostech/dankmemes/fragments/ProfileFragment.kt
+++ app/src/main/java/com/gelostech/dankmemes/fragments/ProfileFragment.kt
@@ -1,24 +1,39 @@
 package com.gelostech.dankmemes.fragments
 
 
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.gelostech.dankmemes.R
+import com.gelostech.dankmemes.activities.ViewMemeActivity
 import com.gelostech.dankmemes.adapters.MemesAdapter
+import com.gelostech.dankmemes.commoners.BaseFragment
+import com.gelostech.dankmemes.commoners.DankMemesUtil
 import com.gelostech.dankmemes.models.MemeModel
+import com.gelostech.dankmemes.models.UserModel
 import com.gelostech.dankmemes.utils.loadUrl
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 
-class ProfileFragment : Fragment(), MemesAdapter.OnItemClickListener {
+class ProfileFragment : BaseFragment(), MemesAdapter.OnItemClickListener {
     private lateinit var memesAdapter: MemesAdapter
+    private lateinit var image: Bitmap
+    private lateinit var user: UserModel
+    private lateinit var profileRef: DatabaseReference
+
+    companion object {
+        private val TAG = ProfileFragment::class.java.simpleName
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -30,6 +45,9 @@ class ProfileFragment : Fragment(), MemesAdapter.OnItemClickListener {
         super.onViewCreated(view, savedInstanceState)
 
         initViews()
+
+        profileRef = getDatabaseReference().child("users").child(getUid())
+        profileRef.addValueEventListener(profileListener)
         loadSample()
     }
 
@@ -41,23 +59,52 @@ class ProfileFragment : Fragment(), MemesAdapter.OnItemClickListener {
         memesAdapter = MemesAdapter(activity!!, this)
         profileRv.adapter = memesAdapter
 
-        profileImage.loadUrl(R.drawable.person)
-        profileName.text = "Vincent Tirgei"
-        profileBio.text = "Here's some random brief summary about me :) "
+        profileImage.setOnClickListener {
+            temporarilySaveImage()
+            startActivity(Intent(activity, ViewMemeActivity::class.java))
+            activity?.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
     }
 
+    private val profileListener = object : ValueEventListener {
+        override fun onCancelled(p0: DatabaseError) {
+            Log.e(TAG, "Error loading profile: ${p0.message}")
+        }
+
+        override fun onDataChange(p0: DataSnapshot) {
+            user = p0.getValue(UserModel::class.java)!!
+
+            profileName.text = user.userName
+            profileBio.text = user.userBio
+            profileImage.loadUrl(user.userAvatar!!)
+        }
+    }
+
+    private fun temporarilySaveImage() {
+        image = (profileImage.drawable as BitmapDrawable).bitmap
+        DankMemesUtil.saveTemporaryImage(activity!!, image)
+    }
+
+    fun getUser() : UserModel = user
+
     private fun loadSample() {
-        val meme1 = MemeModel("1", "Hahahah... ", "fd", R.drawable.games, 23, 45, System.currentTimeMillis(), "Vincent Tirgei")
+        val meme1 = MemeModel("1", "Hahahah... ", "fd", R.drawable.games, 23, 45, System.currentTimeMillis(), "Vincent Tirgei", "AEXdYtUP83MSvrb0aaIqCHN31G23")
         memesAdapter.addMeme(meme1)
 
-        val meme2 = MemeModel("1", "Do y'all agree with this?", "fd", R.drawable.games, 23, 45, System.currentTimeMillis(), "Vincent Tirgei")
+        val meme2 = MemeModel("1", "Do y'all agree with this?", "fd", R.drawable.games, 23, 45, System.currentTimeMillis(), "Vincent Tirgei", "AEXdYtUP83MSvrb0aaIqCHN31G23")
         memesAdapter.addMeme(meme2)
 
-        val meme3 = MemeModel("1", "Ahhhhh... my ribs :D", "fd", R.drawable.games, 23, 45, System.currentTimeMillis(), "Vincent Tirgei")
+        val meme3 = MemeModel("1", "Ahhhhh... my ribs :D", "fd", R.drawable.games, 23, 45, System.currentTimeMillis(), "Vincent Tirgei", "AEXdYtUP83MSvrb0aaIqCHN31G23")
         memesAdapter.addMeme(meme3)
     }
 
     override fun onItemClick(meme: MemeModel, viewID: Int, image: Bitmap?) {
 
     }
+
+    override fun onDestroy() {
+        profileRef.removeEventListener(profileListener)
+        super.onDestroy()
+    }
+
 }
--- app/src/main/java/com/gelostech/dankmemes/models/UserModel.kt
+++ app/src/main/java/com/gelostech/dankmemes/models/UserModel.kt
@@ -1,9 +1,13 @@
 package com.gelostech.dankmemes.models
 
+import java.io.Serializable
+
 data class UserModel(
         var userId: String? = null,
         var userName: String? = null,
         var userToken: String? = null,
         var userAvatar: String? = null,
-        var userEmail: String? = null
-)
+        var userEmail: String? = null,
+        var dateCreated: String? = null,
+        var userBio: String? = null
+): Serializable
