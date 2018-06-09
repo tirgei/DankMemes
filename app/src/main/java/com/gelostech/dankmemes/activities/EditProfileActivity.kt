699f38710f5432de3c7561ac7aab3775ac839cd6
Tirgei <tirgeic@gmail.com>
2018-06-09 12:46:19 +0300

    added functionality to update profile

--- app/src/main/java/com/gelostech/dankmemes/activities/EditProfileActivity.kt
+++ app/src/main/java/com/gelostech/dankmemes/activities/EditProfileActivity.kt
@@ -2,23 +2,36 @@ package com.gelostech.dankmemes.activities
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
 import android.view.MenuItem
 import com.gelostech.dankmemes.R
 import com.gelostech.dankmemes.commoners.BaseActivity
 import com.gelostech.dankmemes.commoners.DankMemesUtil
+import com.gelostech.dankmemes.commoners.DankMemesUtil.drawableToBitmap
+import com.gelostech.dankmemes.commoners.DankMemesUtil.setDrawable
+import com.gelostech.dankmemes.models.UserModel
+import com.gelostech.dankmemes.utils.PreferenceHelper
 import com.gelostech.dankmemes.utils.loadUrl
 import com.mikepenz.ionicons_typeface_library.Ionicons
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.activity_edit_profile.*
+import org.jetbrains.anko.toast
+import com.gelostech.dankmemes.utils.PreferenceHelper.set
 
 class EditProfileActivity : BaseActivity() {
     private var imageUri: Uri? = null
     private var imageSelected = false
+    private var changedAvatar = false
+    private var isUpdating = false
+    private lateinit var updateSuccessful: Bitmap
+    private lateinit var prefs: SharedPreferences
 
     companion object {
         private const val AVATAR_REQUEST = 1
@@ -30,6 +43,9 @@ class EditProfileActivity : BaseActivity() {
         setContentView(R.layout.activity_edit_profile)
 
         initViews()
+        loadProfile()
+
+        prefs = PreferenceHelper.defaultPrefs(this)
     }
 
     private fun initViews() {
@@ -37,6 +53,7 @@ class EditProfileActivity : BaseActivity() {
         supportActionBar?.title = getString(R.string.edit_profile)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        editProfilePickImage.setImageDrawable(DankMemesUtil.setDrawable(this, Ionicons.Icon.ion_camera, R.color.white, 18))
 
         editProfilePickImage.setOnClickListener {
             if (storagePermissionGranted()) {
@@ -48,8 +65,92 @@ class EditProfileActivity : BaseActivity() {
             }
         }
 
-        editProfileImage.loadUrl(R.drawable.prof)
-        editProfilePickImage.setImageDrawable(DankMemesUtil.setDrawable(this, Ionicons.Icon.ion_camera, R.color.white, 18))
+        editProfileButton.setOnClickListener {
+            if (changedAvatar) {
+                updateAvatar()
+            } else {
+                updateDetails()
+            }
+        }
+
+        val successfullyUpdatedIcon = setDrawable(this, Ionicons.Icon.ion_checkmark_round, R.color.white, 25)
+        updateSuccessful = drawableToBitmap(successfullyUpdatedIcon)
+    }
+
+    private fun loadProfile() {
+        val user = intent.getSerializableExtra("user") as UserModel
+
+        editProfileImage.loadUrl(user.userAvatar!!)
+        editProfileName.setText(user.userName)
+        editProfileBio.setText(user.userBio)
+    }
+
+    // User has changed profile picture
+    private fun updateAvatar() {
+        if (!DankMemesUtil.validated(editProfileName, editProfileBio)) return
+
+        editProfileButton.startAnimation()
+        isUpdating = true
+
+        val ref = getStorageReference().child("avatars").child(getUid())
+        val uploadTask = ref.putFile(imageUri!!)
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                throw task.exception!!
+            }
+            Log.d(TAG, "Image uploaded")
+
+            // Continue with the task to get the download URL
+            ref.downloadUrl
+        }.addOnCompleteListener({ task ->
+            if (task.isSuccessful) {
+                val name = editProfileName.text.toString().trim()
+                val bio = editProfileBio.text.toString().trim()
+
+                val userRef = getDatabaseReference().child("users").child(getUid())
+                userRef.child("userName").setValue(name)
+                userRef.child("userBio").setValue(bio)
+                userRef.child("userAvatar").setValue(task.result.toString())
+
+                prefs["username"] = name
+
+                Handler().postDelayed({
+                    editProfileButton.revertAnimation()
+                    isUpdating = false
+
+                    toast("Profile updated!")
+                }, 500)
+
+            } else {
+                editProfileButton.revertAnimation()
+                toast("Error updating profile. Please try again.")
+                Log.d(TAG, "Error updating profile: ${task.exception}")
+            }
+        })
+    }
+
+    // User has updated details only, profile picture still same
+    private fun updateDetails() {
+        if (!DankMemesUtil.validated(editProfileName, editProfileBio)) return
+
+        editProfileButton.startAnimation()
+        isUpdating = true
+
+        val name = editProfileName.text.toString().trim()
+        val bio = editProfileBio.text.toString().trim()
+
+        val userRef = getDatabaseReference().child("users").child(getUid())
+        userRef.child("userName").setValue(name)
+        userRef.child("userBio").setValue(bio)
+
+        prefs["username"] = name
+
+        Handler().postDelayed({
+            editProfileButton.revertAnimation()
+            isUpdating = false
+
+            toast("Profile updated!")
+        }, 500)
     }
 
     private fun startCropActivity(imageUri: Uri) {
@@ -75,6 +176,7 @@ class EditProfileActivity : BaseActivity() {
                 val resultUri = result.uri
 
                 editProfileImage.setImageURI(resultUri)
+                changedAvatar = true
                 imageUri = resultUri
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
@@ -93,7 +195,11 @@ class EditProfileActivity : BaseActivity() {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        overridePendingTransition(R.anim.enter_a, R.anim.exit_b)
+        if (isUpdating) {
+            toast("Please wait...")
+        } else {
+            super.onBackPressed()
+            overridePendingTransition(R.anim.enter_a, R.anim.exit_b)
+        }
     }
 }
