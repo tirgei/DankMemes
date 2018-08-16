16b518e4836170733f745dd86925224ef9e26124
Vincent Tirgei <tirgeic@gmail.com>
2018-08-16 17:42:04 +0300

    added function to fetch all memes

--- app/src/main/java/com/gelostech/dankmemes/fragments/AllFragment.kt
+++ app/src/main/java/com/gelostech/dankmemes/fragments/AllFragment.kt
@@ -1,15 +1,61 @@
 package com.gelostech.dankmemes.fragments
 
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.RelativeLayout
+import com.cocosw.bottomsheet.BottomSheet
 
 import com.gelostech.dankmemes.R
+import com.gelostech.dankmemes.activities.CommentActivity
+import com.gelostech.dankmemes.activities.ProfileActivity
+import com.gelostech.dankmemes.activities.ViewMemeActivity
+import com.gelostech.dankmemes.adapters.MemesAdapter
+import com.gelostech.dankmemes.commoners.AppUtils
+import com.gelostech.dankmemes.commoners.BaseFragment
+import com.gelostech.dankmemes.commoners.Config
+import com.gelostech.dankmemes.models.FaveModel
+import com.gelostech.dankmemes.models.MemeModel
+import com.gelostech.dankmemes.models.ReportModel
+import com.gelostech.dankmemes.utils.RecyclerFormatter
+import com.gelostech.dankmemes.utils.showView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.MutableData
+import com.google.firebase.database.Transaction
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Query
+import com.mopub.nativeads.MoPubNativeAdPositioning
+import com.mopub.nativeads.MoPubRecyclerAdapter
+import com.mopub.nativeads.MoPubStaticNativeAdRenderer
+import com.mopub.nativeads.ViewBinder
+import kotlinx.android.synthetic.main.fragment_all.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.toast
 
-class AllFragment : Fragment() {
+class AllFragment : BaseFragment(), MemesAdapter.OnItemClickListener {
+    private lateinit var memesAdapter: MemesAdapter
+    private lateinit var mopubAdapter: MoPubRecyclerAdapter
+    private lateinit var bs: BottomSheet.Builder
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var lastDocument: DocumentSnapshot
+    private lateinit var loadMoreFooter: RelativeLayout
+
+    companion object {
+        private var TAG = AllFragment::class.java.simpleName
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -17,5 +63,338 @@ class AllFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_all, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        initMopub()
+        allShimmer.startShimmerAnimation()
+
+        loadInitial()
+    }
+
+    private fun initViews() {
+        layoutManager = LinearLayoutManager(activity)
+
+        allRv.setHasFixedSize(true)
+        allRv.layoutManager = layoutManager
+        allRv.addItemDecoration(RecyclerFormatter.DoubleDividerItemDecoration(activity!!))
+        allRv.itemAnimator = DefaultItemAnimator()
+        (allRv.itemAnimator as DefaultItemAnimator).supportsChangeAnimations = false
+
+        memesAdapter = MemesAdapter(activity!!, this)
+
+        loadMoreFooter = allRv.loadMoreFooterView as RelativeLayout
+        allRv.setOnLoadMoreListener {
+            loadMoreFooter.showView()
+            loadMore()
+        }
+
+        allRefresh.setOnRefreshListener {
+            Handler().postDelayed({allRefresh?.isRefreshing = false}, 2500)
+        }
+
+    }
+
+    private fun loadInitial() {
+        getFirestore().collection(Config.MEMES)
+                .orderBy(Config.TIME, Query.Direction.DESCENDING)
+                .limit(12)
+                .addSnapshotListener { p0, p1 ->
+                    hasPosts()
+
+                    if (p1 != null) {
+                        Log.e(TAG, "Error loading initial memes: $p1")
+
+                    }
+
+                    if (p0 == null || p0.isEmpty) {
+                        noPosts()
+                    } else {
+                        lastDocument = p0.documents[p0.size()-1]
+
+                        for (change: DocumentChange in p0.documentChanges) {
+
+                            when(change.type) {
+                                DocumentChange.Type.ADDED -> {
+                                    val meme = change.document.toObject(MemeModel::class.java)
+                                    memesAdapter.addMeme(meme)
+                                }
+
+                                DocumentChange.Type.MODIFIED -> {
+                                    val meme = change.document.toObject(MemeModel::class.java)
+                                    memesAdapter.updateMeme(meme)
+                                }
+
+                                DocumentChange.Type.REMOVED -> {
+                                    val meme = change.document.toObject(MemeModel::class.java)
+                                    memesAdapter.removeMeme(meme)
+                                }
+
+
+                            }
+                        }
+
+                    }
+
+                }
+
+    }
+
+    private fun loadMore() {
+        Log.e(TAG, "Loading from ${memesAdapter.getLastKey()}")
+
+        getFirestore().collection(Config.MEMES)
+                .orderBy(Config.TIME, Query.Direction.DESCENDING)
+                .startAfter(lastDocument)
+                .limit(12)
+                .addSnapshotListener { p0, p1 ->
+                    //hasPosts()
+
+                    if (p1 != null) {
+                        Log.e(TAG, "Error loading more memes: $p1")
+
+                    }
+
+                    if (p0 == null || p0.isEmpty) {
+                        Log.e(TAG, "No more memes")
+                    } else {
+                        lastDocument = p0.documents[p0.size()-1]
+
+                        for (change: DocumentChange in p0.documentChanges) {
+
+                            when(change.type) {
+                                DocumentChange.Type.ADDED -> {
+                                    val meme = change.document.toObject(MemeModel::class.java)
+                                    memesAdapter.addMeme(meme)
+                                }
+
+                                DocumentChange.Type.MODIFIED -> {
+                                    val meme = change.document.toObject(MemeModel::class.java)
+                                    memesAdapter.updateMeme(meme)
+                                }
+
+                                DocumentChange.Type.REMOVED -> {
+                                    val meme = change.document.toObject(MemeModel::class.java)
+                                    memesAdapter.removeMeme(meme)
+                                }
+
+
+                            }
+                        }
+
+                    }
+
+                }
+    }
+
+    private fun initMopub() {
+        val adPositioning = MoPubNativeAdPositioning.MoPubServerPositioning()
+        mopubAdapter = MoPubRecyclerAdapter(activity!!, memesAdapter, adPositioning)
+        mopubAdapter.setContentChangeStrategy(MoPubRecyclerAdapter.ContentChangeStrategy.MOVE_ALL_ADS_WITH_CONTENT)
+
+        val myViewBinder = ViewBinder.Builder(R.layout.item_native)
+                .titleId(R.id.native_title)
+                .textId(R.id.native_text)
+                .mainImageId(R.id.native_main_image)
+                .iconImageId(R.id.native_icon_image)
+                .callToActionId(R.id.native_cta)
+                .privacyInformationIconImageId(R.id.native_privacy_information_icon_image)
+                .build()
+
+        val myRenderer = MoPubStaticNativeAdRenderer(myViewBinder)
+        mopubAdapter.registerAdRenderer(myRenderer)
+        allRv.adapter = mopubAdapter
+        if (isConnected()) mopubAdapter.loadAds(activity!!.getString(R.string.ad_unit_id_native))
+
+    }
+
+    override fun onItemClick(meme: MemeModel, viewID: Int, image: Bitmap?) {
+        when(viewID) {
+            0 -> likePost(meme.id!!)
+            1 -> showBottomSheet(meme, image!!)
+            2 -> favePost(meme.id!!)
+            3 -> showComments(meme)
+            4 -> showMeme(meme, image!!)
+            5 -> showProfile(meme)
+
+        }
+    }
+
+    private fun showProfile(meme: MemeModel) {
+        if (meme.memePosterID != getUid()) {
+            val i = Intent(activity, ProfileActivity::class.java)
+            i.putExtra("userId", meme.memePosterID)
+            startActivity(i)
+            activity?.overridePendingTransition(R.anim.enter_b, R.anim.exit_a)
+        }
+    }
+
+    private fun showMeme(meme: MemeModel, image: Bitmap) {
+        AppUtils.saveTemporaryImage(activity!!, image)
+
+        val i = Intent(activity, ViewMemeActivity::class.java)
+        i.putExtra(Config.PIC_URL, meme.imageUrl)
+        i.putExtra("caption", meme.caption)
+        startActivity(i)
+        AppUtils.fadeIn(activity!!)
+    }
+
+    private fun showBottomSheet(meme: MemeModel, image: Bitmap) {
+        bs = if (getUid() != meme.memePosterID) {
+            BottomSheet.Builder(activity!!).sheet(R.menu.main_bottomsheet)
+        } else {
+            BottomSheet.Builder(activity!!).sheet(R.menu.main_bottomsheet_me)
+        }
+
+        bs.listener { _, which ->
+
+            when(which) {
+                R.id.bs_share -> AppUtils.shareImage(activity!!, image)
+                R.id.bs_delete -> deletePost(meme)
+                R.id.bs_save -> {
+                    if (storagePermissionGranted()) {
+                        AppUtils.saveImage(activity!!, image)
+                    } else requestStoragePermission()
+                }
+                R.id.bs_report -> showReportDialog(meme)
+            }
+
+        }.show()
+
+    }
+
+    private fun showComments(meme: MemeModel) {
+        val i = Intent(activity, CommentActivity::class.java)
+        i.putExtra("memeId", meme.id)
+        startActivity(i)
+        activity?.overridePendingTransition(R.anim.enter_b, R.anim.exit_a)
+    }
+
+    private fun deletePost(meme: MemeModel) {
+        val dbRef = getDatabaseReference().child("dank-memes").child(meme.id!!)
+
+        activity!!.alert("Delete this meme?") {
+            positiveButton("DELETE") {
+                dbRef.removeValue()
+            }
+            negativeButton("CANCEL"){}
+        }.show()
+    }
+
+    private fun likePost(id: String) {
+        getDatabaseReference().child("dank-memes").child(id).runTransaction(object : Transaction.Handler {
+            override fun doTransaction(mutableData: MutableData): Transaction.Result {
+                val meme = mutableData.getValue<MemeModel>(MemeModel::class.java)
+
+                if (meme!!.likes.containsKey(getUid())) {
+                    meme.likesCount = meme.likesCount!! - 1
+                    meme.likes.remove(getUid())
+
+                } else  {
+                    meme.likesCount = meme.likesCount!! + 1
+                    meme.likes[getUid()] = true
+                }
+
+                mutableData.value = meme
+                return Transaction.success(mutableData)
+            }
+
+            override fun onComplete(databaseError: DatabaseError?, b: Boolean, dataSnapshot: DataSnapshot?) {
+
+                Log.d(javaClass.simpleName, "postTransaction:onComplete: $databaseError")
+            }
+        })
+    }
+
+    private fun favePost(id: String) {
+        getDatabaseReference().child("dank-memes").child(id).runTransaction(object : Transaction.Handler {
+            override fun doTransaction(mutableData: MutableData): Transaction.Result {
+                val meme = mutableData.getValue<MemeModel>(MemeModel::class.java)
+
+                if (meme!!.faves.containsKey(getUid())) {
+                    meme.faves.remove(getUid())
+
+                    getDatabaseReference().child("favorites").child(getUid()).child(meme.id!!).removeValue()
+
+                } else  {
+                    meme.faves[getUid()] = true
+
+                    val fave = FaveModel()
+                    fave.faveKey = meme.id!!
+                    fave.commentId = meme.id!!
+                    fave.picUrl = meme.imageUrl!!
+
+                    getDatabaseReference().child("favorites").child(getUid()).child(meme.id!!).setValue(fave)
+                }
+
+                mutableData.value = meme
+                return Transaction.success(mutableData)
+            }
+
+            override fun onComplete(databaseError: DatabaseError?, b: Boolean, dataSnapshot: DataSnapshot?) {
+
+                Log.d(javaClass.simpleName, "postTransaction:onComplete: $databaseError")
+            }
+        })
+    }
+
+    private fun showReportDialog(meme: MemeModel) {
+        val editText = EditText(activity)
+        val layout = FrameLayout(activity)
+        layout.setPaddingRelative(45,15,45,0)
+        layout.addView(editText)
+
+        activity!!.alert("Please provide a reason for reporting") {
+            customView = layout
+
+            positiveButton("REPORT") {
+                if (!AppUtils.validated(editText)) {
+                    activity!!.toast("Please enter a reason to report")
+                    return@positiveButton
+                }
+
+                val key = getDatabaseReference().child("reports").push().key
+                val reason = editText.text.toString().trim()
+
+                val report = ReportModel()
+                report.id = key
+                report.memeId = meme.id
+                report.memePosterId = meme.memePosterID
+                report.reporterId = getUid()
+                report.memeUrl = meme.imageUrl
+                report.reason = reason
+                report.time = System.currentTimeMillis()
+
+                getDatabaseReference().child("reports").child(key!!).setValue(report).addOnCompleteListener {
+                    activity!!.toast("Meme reported!")
+                }
+
+            }
+
+            negativeButton("CANCEL"){}
+        }.show()
+    }
+
+    private fun hasPosts() {
+        allShimmer.stopShimmerAnimation()
+        allShimmer.visibility = View.GONE
+    }
+
+    private fun noPosts() {
+
+    }
+
+    override fun onResume() {
+        mopubAdapter.refreshAds(activity!!.getString(R.string.ad_unit_id_native))
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        mopubAdapter.destroy()
+        super.onDestroy()
+    }
+
+
 
 }
+
