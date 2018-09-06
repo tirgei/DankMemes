2bd352dadae4883fd3fc8393da6801a4ecf182f2
Vincent Tirgei <tirgeic@gmail.com>
2018-09-07 00:24:25 +0300

    fixed user avatar bug

--- app/src/main/java/com/gelostech/dankmemes/models/NotificationModel.kt
+++ app/src/main/java/com/gelostech/dankmemes/models/NotificationModel.kt
@@ -4,7 +4,7 @@ data class NotificationModel(
         var type: Int? = null,
         var id: String? = null,
         var userId: String? = null,
-        var userAvatar: Int? = null,
+        var userAvatar: String? = null,
         var username: String? = null,
         var memeId: String? = null,
         var time: Long? = null,
