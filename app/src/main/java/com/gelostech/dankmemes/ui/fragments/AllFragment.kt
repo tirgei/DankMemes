8d51b12dc06a60fa3ed2f02bc447a5898614f07b
Tirgei <vincent.tirgei@tutanota.com>
2019-10-18 14:09:17 +0300

    Update ad ID name

--- app/src/main/java/com/gelostech/dankmemes/ui/fragments/AllFragment.kt
+++ app/src/main/java/com/gelostech/dankmemes/ui/fragments/AllFragment.kt
@@ -163,7 +163,7 @@ class AllFragment : BaseFragment(), MemesAdapter.OnItemClickListener {
         val myRenderer = MoPubStaticNativeAdRenderer(myViewBinder)
         mopubAdapter.registerAdRenderer(myRenderer)
         allRv.adapter = mopubAdapter
-        if (isConnected()) mopubAdapter.loadAds(activity!!.getString(R.string.mopup_native_ad))
+        if (isConnected()) mopubAdapter.loadAds(activity!!.getString(R.string.mopub_native_ad))
 
     }
 
