1b9b2cca98cce23e42074b2b22f0e21aa0895893
Vincent Tirgei <tirgeic@gmail.com>
2019-10-21 19:27:11 +0300

    Initialize Koin

--- app/src/main/java/com/gelostech/dankmemes/DankMemes.kt
+++ app/src/main/java/com/gelostech/dankmemes/DankMemes.kt
@@ -3,9 +3,15 @@ package com.gelostech.dankmemes
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.database.FirebaseDatabase
 import cat.ereza.customactivityoncrash.config.CaocConfig
-import com.gelostech.dankmemes.R
+import com.gelostech.dankmemes.di.firebaseModule
+import com.gelostech.dankmemes.di.repositoriesModule
+import com.gelostech.dankmemes.di.sessionManagerModule
+import com.gelostech.dankmemes.di.viewModelsModule
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 
@@ -29,5 +35,12 @@ class DankMemes : MultiDexApplication() {
                 .showErrorDetails(false)
                 .errorDrawable(R.mipmap.ic_launcher)
                 .apply()
+
+        // Initialize Koin
+        startKoin {
+            androidLogger()
+            androidContext(this@DankMemes)
+            modules(listOf(firebaseModule, repositoriesModule, viewModelsModule, sessionManagerModule))
+        }
     }
 }
