ebcfb3412d5ca35c3e3906a888e2e2aa560ed90c
Tirgei <vincent.tirgei@tutanota.com>
2019-10-25 10:31:17 +0300

    Fix FirebaseApp initialization and enable CustomActivityOnCrash

--- app/src/main/java/com/gelostech/dankmemes/DankMemes.kt
+++ app/src/main/java/com/gelostech/dankmemes/DankMemes.kt
@@ -4,6 +4,7 @@ import androidx.multidex.MultiDexApplication
 import com.google.firebase.database.FirebaseDatabase
 import cat.ereza.customactivityoncrash.config.CaocConfig
 import com.gelostech.dankmemes.di.*
+import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import org.koin.android.ext.koin.androidContext
@@ -17,21 +18,25 @@ class DankMemes : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
+        // Initialize app
+        if (FirebaseApp.getApps(this).isNullOrEmpty())
+            FirebaseApp.initializeApp(this)
+
         // Set Timber
         Timber.plant(Timber.DebugTree())
+
+        // Initialize crash activity
+        CaocConfig.Builder.create()
+                .enabled(true)
+                .showErrorDetails(false)
+                .errorDrawable(R.mipmap.ic_launcher)
+                .apply()
 
         // Enable Firebase persistence
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         FirebaseFirestore.getInstance().firestoreSettings = FirebaseFirestoreSettings.Builder()
                 .setPersistenceEnabled(true)
                 .build()
-
-        // Initialize crash activity
-        CaocConfig.Builder.create()
-                .enabled(false)
-                .showErrorDetails(false)
-                .errorDrawable(R.mipmap.ic_launcher)
-                .apply()
 
         // Initialize Koin
         startKoin {
