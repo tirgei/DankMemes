2a82708363366d089754344b7d22c9a0c4c44411
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-11-08 14:46:54 +0300

    Add item field

--- app/src/main/java/com/gelostech/dankmemes/data/responses/GenericResponse.kt
+++ app/src/main/java/com/gelostech/dankmemes/data/responses/GenericResponse.kt
@@ -5,14 +5,23 @@ import com.gelostech.dankmemes.data.Status
 data class GenericResponse (
         val status: Status,
         val success: Boolean?,
-        val error: String?
+        val error: String?,
+        val item: ITEM_RESPONSE?
 ) {
     companion object {
-        fun loading(): GenericResponse = GenericResponse(Status.LOADING, null, null)
+        fun loading(): GenericResponse = GenericResponse(Status.LOADING, null, null, null)
 
-        fun success(success: Boolean): GenericResponse
-                = GenericResponse(Status.SUCCESS, success, null)
+        fun success(success: Boolean, item: ITEM_RESPONSE): GenericResponse
+                = GenericResponse(Status.SUCCESS, success, null, item)
 
-        fun error(error: String): GenericResponse = GenericResponse(Status.ERROR, null, error)
+        fun error(error: String): GenericResponse = GenericResponse(Status.ERROR, null, error, null)
+    }
+
+    enum class ITEM_RESPONSE {
+        POST_MEME,
+        LIKE_MEME,
+        FAVE_MEME,
+        DELETE_MEME,
+        RESET_PASSWORD
     }
 }
