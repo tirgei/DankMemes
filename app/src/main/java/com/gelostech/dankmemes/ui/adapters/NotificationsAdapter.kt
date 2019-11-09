40d761f0b2ccfd43fa43fee40b308eebb14331aa
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-11-09 16:43:16 +0300

    Refactor NotificationsAdapter to PagedList Adapter

--- app/src/main/java/com/gelostech/dankmemes/ui/adapters/NotificationsAdapter.kt
+++ app/src/main/java/com/gelostech/dankmemes/ui/adapters/NotificationsAdapter.kt
@@ -1,93 +1,49 @@
 package com.gelostech.dankmemes.ui.adapters
 
-import android.view.View
 import android.view.ViewGroup
+import androidx.paging.PagedList
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.gelostech.dankmemes.R
-import com.gelostech.dankmemes.ui.callbacks.NotificationsCallback
-import com.gelostech.dankmemes.utils.AppUtils
 import com.gelostech.dankmemes.data.models.Notification
 import com.gelostech.dankmemes.databinding.ItemNotificationBinding
+import com.gelostech.dankmemes.ui.callbacks.NotificationsCallback
 import com.gelostech.dankmemes.utils.TimeFormatter
 import com.gelostech.dankmemes.utils.inflate
-import com.gelostech.dankmemes.utils.load
-import kotlinx.android.synthetic.main.item_notification.view.*
+import timber.log.Timber
 
-class NotificationsAdapter(private val callback:NotificationsCallback) : RecyclerView.Adapter<NotificationsAdapter.NotificationHolder>() {
-    private val notifications = mutableListOf<Notification>()
 
-    fun addNotification(notification: Notification) {
-        if (!hasBeenAdded(notification)) {
-            if (!isNewer(notification)) {
-                notifications.add(notification)
-                notifyItemInserted(notifications.size-1)
+class NotificationsAdapter(private val callback: NotificationsCallback): PagedListAdapter<Notification, 
+        NotificationsAdapter.NotificationHolder>(DIFF_CALLBACK) {
 
-            } else {
-                notifications.add(0, notification)
-                notifyItemInserted(0)
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Notification>() {
+            override fun areItemsTheSame(oldItem: Notification, newItem: Notification): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Notification, newItem: Notification): Boolean {
+                return oldItem == newItem
             }
         }
     }
 
-    fun updateNotif(notification: Notification) {
-        for ((index, n) in notifications.withIndex()) {
-            if (n.equals(notification)) {
-                notifications[index] = notification
-                notifyItemChanged(index, notification)
-            }
-        }
-    }
-
-    fun removeNotif(removedNotif: Notification) {
-        var indexToRemove = -1
-
-        for ((index, notif) in notifications.withIndex()) {
-            if (notif.equals(removedNotif)) {
-                indexToRemove = index
-            }
-        }
-
-        notifications.removeAt(indexToRemove)
-        notifyItemRemoved(indexToRemove)
-    }
-
-    private fun hasBeenAdded(notification: Notification): Boolean {
-        var added = false
-
-        for (n in notifications) {
-            if (n.equals(notification)) {
-                added = true
-            }
-        }
-
-        return added
-    }
-
-    private fun isNewer(notification: Notification): Boolean {
-        var newer = false
-
-        if (notifications.size > 0) {
-            val n = notifications[0]
-
-            if (n.time!! < notification.time!!) {
-                newer = true
-            }
-        }
-
-        return newer
+    override fun onCurrentListChanged(previousList: PagedList<Notification>?, currentList: PagedList<Notification>?) {
+        super.onCurrentListChanged(previousList, currentList)
+        Timber.e("Previous list: $previousList vs Current list: $currentList")
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationHolder {
         return NotificationHolder(parent.inflate(R.layout.item_notification), callback)
     }
 
-    override fun getItemCount(): Int = notifications.size
-
     override fun onBindViewHolder(holder: NotificationHolder, position: Int) {
-        holder.bind(notifications[position])
+        val notification = getItem(position)
+        holder.bind(notification!!)
     }
 
-    class NotificationHolder(private val binding: ItemNotificationBinding, private val callback: NotificationsCallback):
+    inner class NotificationHolder(private val binding: ItemNotificationBinding, private val callback: NotificationsCallback):
             RecyclerView.ViewHolder(binding.root) {
 
         fun bind(notification: Notification) {
@@ -96,5 +52,4 @@ class NotificationsAdapter(private val callback:NotificationsCallback) : Recycle
             binding.timeFormatter = TimeFormatter()
         }
     }
-
 }
