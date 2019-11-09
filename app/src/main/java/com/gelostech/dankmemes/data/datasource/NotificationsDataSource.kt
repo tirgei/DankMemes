e4694d3f95c156599446564fcdd8a6982f07fdd8
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-11-09 16:42:38 +0300

    Add function to fetch Notifications

---
+++ app/src/main/java/com/gelostech/dankmemes/data/datasource/NotificationsDataSource.kt
@@ -0,0 +1,54 @@
+package com.gelostech.dankmemes.data.datasource
+
+import androidx.paging.DataSource
+import androidx.paging.ItemKeyedDataSource
+import com.gelostech.dankmemes.data.models.Notification
+import com.gelostech.dankmemes.data.repositories.NotificationsRepository
+import com.gelostech.dankmemes.utils.SessionManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import timber.log.Timber
+
+class NotificationsDataSource constructor(private val repository: NotificationsRepository,
+                                  private val scope: CoroutineScope): ItemKeyedDataSource<String, Notification>(), KoinComponent {
+
+    private val sessionManager: SessionManager by inject()
+    private val userId = sessionManager.getUserId()
+
+    class Factory(private val repository: NotificationsRepository,
+                  private val scope: CoroutineScope): DataSource.Factory<String, Notification>() {
+        override fun create(): DataSource<String, Notification> {
+            return NotificationsDataSource(repository, scope)
+        }
+    }
+
+    override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<Notification>) {
+        Timber.e("Loading initial...")
+
+        scope.launch {
+            val notifications = repository.fetchNotifications(userId)
+            Timber.e("Notifications fetched: ${notifications.size}")
+            callback.onResult(notifications)
+        }
+    }
+
+    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<Notification>) {
+        scope.launch {
+            val notifications = repository.fetchNotifications(userId = userId, loadAfter = params.key)
+            Timber.e("Notifications fetched: ${notifications.size}")
+            callback.onResult(notifications)
+        }
+    }
+
+    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<Notification>) {
+        scope.launch {
+            val notifications = repository.fetchNotifications(userId = userId, loadBefore = params.key)
+            Timber.e("Notifications fetched: ${notifications.size}")
+            callback.onResult(notifications)
+        }
+    }
+
+    override fun getKey(item: Notification): String = item.id!!
+}
--- app/src/main/java/com/gelostech/dankmemes/data/repositories/NotificationsRepository.kt
+++ app/src/main/java/com/gelostech/dankmemes/data/repositories/NotificationsRepository.kt
@@ -1,47 +1,33 @@
 package com.gelostech.dankmemes.data.repositories
 
-import com.gelostech.dankmemes.data.Result
 import com.gelostech.dankmemes.data.models.Notification
 import com.gelostech.dankmemes.utils.Constants
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 
 class NotificationsRepository constructor(private val firestoreDatabase: FirebaseFirestore) {
-    private val userId: String = ""
-    private val db = firestoreDatabase
-            .collection(Constants.NOTIFICATIONS)
-            .document(userId)
-            .collection(Constants.USER_NOTIFS)
 
-    private val initialQuery = db.orderBy(Constants.TIME, Query.Direction.DESCENDING).limit(Constants.MEMES_COUNT)
-    private var nextQuery: Query? = null
+    /**
+     * Fetch all notifications
+     */
+    suspend fun fetchNotifications(userId: String, loadBefore: String? = null, loadAfter: String? = null): List<Notification> {
+        Timber.e("Fetching memes...")
+        val db = firestoreDatabase.collection(Constants.NOTIFICATIONS).document(userId).collection(Constants.USER_NOTIFS)
+        var query = db.orderBy(Constants.TIME, Query.Direction.DESCENDING).limit(Constants.MEMES_COUNT)
 
-    fun fetchNotifications(onResult: (Result<List<Notification>>) -> Unit) {
-        if (nextQuery != null) {
-            nextQuery!!.get()
-                    .addOnSuccessListener { querySnapshot ->
-                        val notifications = handleFetchedData(querySnapshot)
-                        onResult(Result.Success(notifications))
-                    }
-                    .addOnFailureListener { onResult(Result.Error("Error loading notifications")) }
-        } else {
-            initialQuery.get()
-                    .addOnSuccessListener { querySnapshot ->
-                        val notifications = handleFetchedData(querySnapshot)
-                        onResult(Result.Success(notifications))
-                    }
-                    .addOnFailureListener { onResult(Result.Error("Error loading notifications")) }
+        loadBefore?.let {
+            val notif = db.document(it).get().await()
+            query = query.endBefore(notif)
         }
-    }
 
-    private fun handleFetchedData(querySnapshot: QuerySnapshot): List<Notification> {
-        val lastFetchedMeme = querySnapshot.documents[querySnapshot.size()-1]
-        nextQuery = initialQuery.startAfter(lastFetchedMeme)
-
-        return querySnapshot.map { snapshot ->
-            snapshot.toObject(Notification::class.java)
+        loadAfter?.let {
+            val notif = db.document(it).get().await()
+            query = query.startAfter(notif)
         }
+
+        return query.get().await().map { it.toObject(Notification::class.java) }
     }
 
 }
