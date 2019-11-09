00598f332deebf8048bea8601ada2ddf02c5cb1f
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-11-09 20:14:38 +0300

    Add user profile callback

--- app/src/main/java/com/gelostech/dankmemes/ui/callbacks/MemesCallback.kt
+++ app/src/main/java/com/gelostech/dankmemes/ui/callbacks/MemesCallback.kt
@@ -3,8 +3,9 @@ package com.gelostech.dankmemes.ui.callbacks
 import android.view.View
 import com.gelostech.dankmemes.data.models.Meme
 import com.gelostech.dankmemes.data.models.User
+import de.hdodenhof.circleimageview.CircleImageView
 
 interface MemesCallback {
     fun onMemeClicked(view: View, meme: Meme)
-    fun onProfileClicked(user: User)
+    fun onProfileClicked(view: View, user: User)
 }
