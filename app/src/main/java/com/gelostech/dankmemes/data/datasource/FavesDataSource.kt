15dc2c9a809b0daa25246520fb49fd67f3cc36c9
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-11-09 14:56:10 +0300

    Add data source for faves

---
+++ app/src/main/java/com/gelostech/dankmemes/data/datasource/FavesDataSource.kt
@@ -0,0 +1,54 @@
+package com.gelostech.dankmemes.data.datasource
+
+import androidx.paging.DataSource
+import androidx.paging.ItemKeyedDataSource
+import com.gelostech.dankmemes.data.models.Fave
+import com.gelostech.dankmemes.data.repositories.MemesRepository
+import com.gelostech.dankmemes.utils.SessionManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import timber.log.Timber
+
+class FavesDataSource constructor(private val repository: MemesRepository,
+                                  private val scope: CoroutineScope): ItemKeyedDataSource<String, Fave>(), KoinComponent {
+
+    private val sessionManager: SessionManager by inject()
+    private val userId = sessionManager.getUserId()
+
+    class Factory(private val repository: MemesRepository,
+                  private val scope: CoroutineScope): DataSource.Factory<String, Fave>() {
+        override fun create(): DataSource<String, Fave> {
+            return FavesDataSource(repository, scope)
+        }
+    }
+
+    override fun loadInitial(params: LoadInitialParams<String>, callback: LoadInitialCallback<Fave>) {
+        Timber.e("Loading initial...")
+
+        scope.launch {
+            val memes = repository.fetchFaves(userId)
+            Timber.e("Faves fetched: ${memes.size}")
+            callback.onResult(memes)
+        }
+    }
+
+    override fun loadAfter(params: LoadParams<String>, callback: LoadCallback<Fave>) {
+        scope.launch {
+            val memes = repository.fetchFaves(userId = userId, loadAfter = params.key)
+            Timber.e("Faves fetched: ${memes.size}")
+            callback.onResult(memes)
+        }
+    }
+
+    override fun loadBefore(params: LoadParams<String>, callback: LoadCallback<Fave>) {
+        scope.launch {
+            val memes = repository.fetchFaves(userId = userId, loadBefore = params.key)
+            Timber.e("Faves fetched: ${memes.size}")
+            callback.onResult(memes)
+        }
+    }
+
+    override fun getKey(item: Fave): String = item.id!!
+}
