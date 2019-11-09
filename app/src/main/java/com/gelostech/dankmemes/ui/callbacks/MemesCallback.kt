d340267432111b03366939abf75a5d402024b130
Vincent Tirgei <vincent.tirgei@tutanota.com>
2019-11-09 19:16:04 +0300

    Add profile callback

--- app/src/main/java/com/gelostech/dankmemes/ui/callbacks/MemesCallback.kt
+++ app/src/main/java/com/gelostech/dankmemes/ui/callbacks/MemesCallback.kt
@@ -2,7 +2,9 @@ package com.gelostech.dankmemes.ui.callbacks
 
 import android.view.View
 import com.gelostech.dankmemes.data.models.Meme
+import com.gelostech.dankmemes.data.models.User
 
 interface MemesCallback {
     fun onMemeClicked(view: View, meme: Meme)
+    fun onProfileClicked(user: User)
 }
