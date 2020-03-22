c5b32d8e791c7ca79ffa29c846a184a1f990901d
Vincent Tirgei <tirgeic@gmail.com>
2020-03-22 15:01:27 +0300

    Add layout for adding memes activity

--- app/src/main/java/com/gelostech/dankmemes/ui/activities/AddMemesActivity.kt
+++ app/src/main/java/com/gelostech/dankmemes/ui/activities/AddMemesActivity.kt
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gelostech.dankmemes.R
 import com.gelostech.dankmemes.ui.base.BaseActivity
+import com.gelostech.dankmemes.utils.AppUtils
+import kotlinx.android.synthetic.main.activity_add_memes.*
 
 class AddMemesActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_memes)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = null
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        AppUtils.slideLeft(this)
     }
 }
